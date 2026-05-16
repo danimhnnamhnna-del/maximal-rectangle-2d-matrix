b2c138fdd19ae6bd1fff44cc3fbcde22d94aec19
yourName <email@gmail.com>
2026-05-16 21:32:09 +0200

    implement 2D matrix dynamic scanning and cumulative row tracking

--- src/maximal-rectangle.ts
+++ src/maximal-rectangle.ts
@@ -21,3 +21,34 @@ function largestRectangleArea(heights: number[]): number {
   }
   return maxArea;
 }
+function maximalRectangle(matrix: number[][]): number {
+  if (matrix.length === 0 || matrix[0]!.length === 0) {
+    return 0;
+  }
+  const columnsCount = matrix[0]!.length;
+  const histogramHeights = new Array(columnsCount).fill(0);
+  let maxRectangleArea = 0;
+  for (let row = 0; row < matrix.length; row++) {
+    for (let col = 0; col < columnsCount; col++) {
+      if (matrix[row]![col] === 1) {
+        histogramHeights[col] += 1;
+      } else {
+        histogramHeights[col] = 0;
+      }
+    }
+    const currentRowsMaxArea = largestRectangleArea(histogramHeights);
+    if (currentRowsMaxArea > maxRectangleArea) {
+      maxRectangleArea = currentRowsMaxArea;
+    }
+  }
+  return maxRectangleArea;
+}
+
+const matrix = [
+  [0, 1, 1, 0],
+  [1, 1, 1, 1],
+  [1, 1, 1, 1],
+  [1, 1, 0, 0],
+];
+
+console.log(maximalRectangle(matrix));
